cc7aa84804ea13d75ac2e62e4320fec4cffffded
KareemMohamed95 <38453431+KareemMohamed95@users.noreply.github.com>
2021-08-23 12:31:08 +0200

    Update latex2r.R

--- R/latex2r.R
+++ R/latex2r.R
@@ -50,7 +50,6 @@ latex2r <- function(l) {
 
 #' @export
 r2latex <- function(r) { 
-  
   str2 <- Ryacas::yac_str(paste0("TexForm(", r, ")"))
   str2 <- gsub("$", "", str2, fixed = TRUE)
   str2 <- gsub("\n", "", str2, fixed = TRUE)
